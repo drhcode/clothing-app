67b9acf968e5d2b16c50eea5dacffbc5986f3c61
drhcode <doriancocka@gmail.com>
2023-01-23 16:06:53 +0100

    Added header and installed firebase

---
+++ src/components/routes/home/navigation/navigation.jsx
@@ -0,0 +1,30 @@
+import { Outlet, Link } from 'react-router-dom';
+import { Fragment } from 'react';
+
+import { ReactComponent as ShopLogo } from '../../../../assets/crown.svg';
+
+import './navigation.scss'
+
+const Navigation = () => {
+    return (
+    <Fragment>
+      <div className='navigation'>
+
+          <Link className='logo-container' to='/' >
+            <ShopLogo className='logo'/>
+          </Link>
+          <div className='nav-links-container'>
+            <Link className='nav-link' to='/shop'>
+                Shop
+            </Link>
+            <Link className='nav-link' to='/sign-in'>
+                Sign In
+            </Link>
+          </div>
+        </div>
+        <Outlet />
+    </Fragment>
+    )
+  }
+
+  export default Navigation
